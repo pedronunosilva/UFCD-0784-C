5834bdee8326e93ddf357f1f5b5ba21a84cd6fe4
Pedro Nuno Silva <12299@ufp.edu.pt>
2016-10-19 21:59:18 +0100

    update

---
+++ Exercícios/ex15.c
@@ -0,0 +1,104 @@
+//
+//  ex15.c
+//  CICCOPN C
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void cifrar (char *mensagem, int tamanho);
+int procurarChar (char c, char *string, int tamanho);
+
+
+int main( int argc, char *argv[] )
+{
+
+    /* declaração de variáveis */
+    int i = 0, tamanho = 0;
+    
+    char *str = NULL;
+    
+
+    if(argc < 2) {
+
+        printf("Argumentos insuficientes");
+
+    } else if (argc == 2) {
+
+        printf("Mensagem: %s \n", argv[1]);
+        
+        tamanho = (unsigned int) strlen ( argv[1]);
+        
+        cifrar(argv[1], tamanho);
+
+
+    } else {
+
+        str = (char *) malloc((unsigned int) strlen ( argv[1]) + 1 );
+
+        strcpy (str, argv[1]);
+
+        for(i = 2; i < argc; i++){
+
+            str = (char *) realloc(str, (unsigned int) strlen ( argv[i]) + (unsigned int) strlen (str) + 1);
+
+            strcat (str, " ");
+
+            strcat (str, argv[i]);
+
+        }
+
+        tamanho = (unsigned int) strlen (str);
+        
+        cifrar(str, tamanho);
+
+    }
+
+    return(0);
+}
+
+
+void cifrar (char *msg, int tamanho){
+
+    /* declaração de variáveis */
+    char *input = {" ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
+    
+    char *output ={" NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
+    
+    char *mensagem = NULL;
+
+    mensagem = (char*) malloc (sizeof(char) * tamanho);
+
+    int posicao = 0, i = 0;
+    
+    printf("\n\n || %s || \n\n", msg);
+    
+    for (i = 0; i <= tamanho; i++){
+    
+    
+        posicao = procurarChar(msg[i], input, 53);
+        
+        mensagem[i] = output[posicao];
+    }
+    
+    printf("A mensagem codificada e: '%s'\n", mensagem);
+
+}
+
+int procurarChar (char c, char *string, int tamanho){
+
+    int pos = 0;
+
+
+    while (c != string[pos] && string[pos]!='\0' ) {
+
+        pos++;
+
+    }
+
+    return pos;
+
+}
+
